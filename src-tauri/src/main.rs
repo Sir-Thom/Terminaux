97a2171553fec317308eaa7edd96b54abe34a562
Sir-Thom <Sir-Thom1702@proton.me>
2023-06-01 11:03:17 -0400

    First commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,95 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use portable_pty::{native_pty_system, CommandBuilder, PtyPair, PtySize};
+use std::{
+    io::{BufRead, BufReader, Write},
+    sync::{Arc, Mutex},
+    thread::{self, sleep},
+    time::Duration,
+};
+use tauri::{async_runtime::Mutex as AsyncMutex, State, WindowBuilder};
+
+struct AppState {
+    pty_pair: Arc<AsyncMutex<PtyPair>>,
+    writer: Arc<AsyncMutex<Box<dyn Write + Send>>>,
+}
+
+#[tauri::command]
+async fn async_write_to_pty(data: &str, state: State<'_, AppState>) -> Result<(), ()> {
+    write!(state.writer.lock().await, "{}", data).map_err(|_| ())
+}
+
+#[tauri::command]
+async fn async_resize_pty(rows: u16, cols: u16, state: State<'_, AppState>) -> Result<(), ()> {
+    state
+        .pty_pair
+        .lock()
+        .await
+        .master
+        .resize(PtySize {
+            rows,
+            cols,
+            ..Default::default()
+        })
+        .map_err(|_| ())
+}
+
+fn main() {
+    let pty_system = native_pty_system();
+
+    let pty_pair = pty_system
+        .openpty(PtySize {
+            rows: 80,
+            cols: 48,
+            pixel_width: 0,
+            pixel_height: 0,
+        })
+        .unwrap();
+
+    #[cfg(target_os = "windows")]
+    let cmd = CommandBuilder::new("powershell.exe");
+    #[cfg(not(target_os = "windows"))]
+    let cmd = CommandBuilder::new("bash");
+
+    let mut child = pty_pair.slave.spawn_command(cmd).unwrap();
+
+    thread::spawn(move || {
+        child.wait().unwrap();
+    });
+
+    let reader = pty_pair.master.try_clone_reader().unwrap();
+    let writer = pty_pair.master.take_writer().unwrap();
+
+    let reader = Arc::new(Mutex::new(Some(BufReader::new(reader))));
+
+    tauri::Builder::default()
+        .on_page_load(move |window, _| {
+            let window = window.clone();
+            let reader = reader.clone();
+            let window_size = window.inner_size().unwrap();
+            println!("Window size: {}x{}", window_size.width, window_size.height);
+            thread::spawn(move || {
+                let reader = reader.lock().unwrap().take();
+                if let Some(mut reader) = reader {
+                    loop {
+                        sleep(Duration::from_millis(1));
+                        let mut data = Vec::new();
+                        if reader.read_until(b'\n', &mut data).unwrap() > 0 {
+                            window.emit("data", data).unwrap();
+                        }
+                    }
+                }
+            });
+        })
+        .manage(AppState {
+            pty_pair: Arc::new(AsyncMutex::new(pty_pair)),
+            writer: Arc::new(AsyncMutex::new(writer)),
+        })
+        .invoke_handler(tauri::generate_handler![
+            async_write_to_pty,
+            async_resize_pty
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
